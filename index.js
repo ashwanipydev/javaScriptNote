9cee2f48b64c3d0f960a7c20e1291f30441cf442
ashwani <pydev.ashwani@gmail.com>
2023-08-16 13:34:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// let word = 'JavaScript'
+// word[0] = 'Y'
+// console.log(word);
+
+// let numOne = 4
+// let numTwo = 3
+// let sum = numOne + numTwo
+// let diff = numOne - numTwo
+// let mult = numOne * numTwo
+// let div = numOne / numTwo
+// let remainder = numOne % numTwo
+// let powerOf = numOne ** numTwo
+
+// console.log(sum, diff, mult, div, remainder, powerOf) // 7,1,12,1.33,1, 64
+
+// console.log(3 > 2)              // true, because 3 is greater than 2
+// console.log(3 >= 2)             // true, because 3 is greater than 2
+// console.log(3 < 2)              // false,  because 3 is greater than 2
+// console.log(2 < 3)              // true, because 2 is less than 3
+// console.log(2 <= 3)             // true, because 2 is less than 3
+// console.log(3 == 2)             // false, because 3 is not equal to 2
+// console.log(3 != 2)             // true, because 3 is not equal to 2
+// console.log(3 == '3')           // true, compare only value
+// console.log(3 === '3')          // false, compare both value and data type
+// console.log(3 !== '3')          // true, compare both value and data type
+// console.log(3 != 3)             // false, compare only value
+// console.log(3 !== 3)            // false, compare both value and data type
+// console.log(0 == false)         // true, equivalent
+// console.log(0 === false)        // false, not exactly the same
+// console.log(0 == '')            // true, equivalent
+// console.log(0 == ' ')           // true, equivalent
+// console.log(0 === '')           // false, not exactly the same
+// console.log(1 == true)          // true, equivalent
+// console.log(1 === true)         // false, not exactly the same
+// console.log(undefined == null)  // true
+// console.log(undefined === null) // false
+// console.log(NaN == NaN)         // false, not equal
+// console.log(NaN === NaN)        // false
+// console.log(typeof NaN)         // number
+
+// console.log('mango'.length == 'avocado'.length)  // false
+// console.log('mango'.length != 'avocado'.length)  // true
+// console.log('mango'.length < 'avocado'.length)   // true
+// console.log('milk'.length == 'meat'.length)      // true
+// console.log('milk'.length != 'meat'.length)      // false
+// console.log('tomato'.length == 'potato'.length)  // true
+// console.log('python'.length > 'dragon'.length)   // false
+
+
+// const now = new Date()
+// console.log(now.toLocaleString()) // 8/16/2023, 11:00:36 AM
+
+// const now = new Date()
+// console.log(now.getDate()) // 16, because the day of the month is 4th,  day(1-31)
+
+// Let us access the arguments object
+// function sumAllNums() {
+//  console.log(typeof arguments, arguments)
+// }
+
+// sumAllNums(1, 2, 3, 4)
+// // Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
