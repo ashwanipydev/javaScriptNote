e384bc0588dec24519c31eb4ef0b2a92a66c50bc
ashwani <pydev.ashwani@gmail.com>
2023-08-19 12:09:15 +0530

    done

--- index.js
+++ index.js
@@ -1,62 +1,53 @@
-// let word = 'JavaScript'
-// word[0] = 'Y'
-// console.log(word);
+// stack , heep 
 
-// let numOne = 4
-// let numTwo = 3
-// let sum = numOne + numTwo
-// let diff = numOne - numTwo
-// let mult = numOne * numTwo
-// let div = numOne / numTwo
-// let remainder = numOne % numTwo
-// let powerOf = numOne ** numTwo
+// let myName = 'Ankit'
+// let myFullName = myName
 
-// console.log(sum, diff, mult, div, remainder, powerOf) // 7,1,12,1.33,1, 64
+// console.log(myName); //Ankit
+// console.log(myFullName); //Ankit
 
-// console.log(3 > 2)              // true, because 3 is greater than 2
-// console.log(3 >= 2)             // true, because 3 is greater than 2
-// console.log(3 < 2)              // false,  because 3 is greater than 2
-// console.log(2 < 3)              // true, because 2 is less than 3
-// console.log(2 <= 3)             // true, because 2 is less than 3
-// console.log(3 == 2)             // false, because 3 is not equal to 2
-// console.log(3 != 2)             // true, because 3 is not equal to 2
-// console.log(3 == '3')           // true, compare only value
-// console.log(3 === '3')          // false, compare both value and data type
-// console.log(3 !== '3')          // true, compare both value and data type
-// console.log(3 != 3)             // false, compare only value
-// console.log(3 !== 3)            // false, compare both value and data type
-// console.log(0 == false)         // true, equivalent
-// console.log(0 === false)        // false, not exactly the same
-// console.log(0 == '')            // true, equivalent
-// console.log(0 == ' ')           // true, equivalent
-// console.log(0 === '')           // false, not exactly the same
-// console.log(1 == true)          // true, equivalent
-// console.log(1 === true)         // false, not exactly the same
-// console.log(undefined == null)  // true
-// console.log(undefined === null) // false
-// console.log(NaN == NaN)         // false, not equal
-// console.log(NaN === NaN)        // false
-// console.log(typeof NaN)         // number
+// // "But if we change the `myFullName` variable, it won't affect the original data stored in `myName`."
 
-// console.log('mango'.length == 'avocado'.length)  // false
-// console.log('mango'.length != 'avocado'.length)  // true
-// console.log('mango'.length < 'avocado'.length)   // true
-// console.log('milk'.length == 'meat'.length)      // true
-// console.log('milk'.length != 'meat'.length)      // false
-// console.log('tomato'.length == 'potato'.length)  // true
-// console.log('python'.length > 'dragon'.length)   // false
+// myFullName = 'Ashwani pandey'
+// console.log(myFullName);
+// console.log(myName);
 
 
-// const now = new Date()
-// console.log(now.toLocaleString()) // 8/16/2023, 11:00:36 AM
-
-// const now = new Date()
-// console.log(now.getDate()) // 16, because the day of the month is 4th,  day(1-31)
-
-// Let us access the arguments object
-// function sumAllNums() {
-//  console.log(typeof arguments, arguments)
+// const userOne ={
+//     myName: 'Ashwnai',
+//     myLastName: 'Pandey',
+//     email: 'email@xyz.com'
 // }
 
-// sumAllNums(1, 2, 3, 4)
-// // Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+// let userTow = userOne
+
+// console.log(userOne); //{ myName: 'Ashwnai', myLastName: 'Pandey', email: 'email@xyz.com' }
+// console.log(userTow); //{ myName: 'Ashwnai', myLastName: 'Pandey', email: 'email@xyz.com' }
+
+// "But if we change the `userTow` variable, it affect the original data stored in `userOne`."
+
+// userTow.email = "abc@abc.com"
+
+// console.log(userTow); //{ myName: 'Ashwnai', myLastName: 'Pandey', email: 'abc@abc.com' }
+// console.log(userOne); //{ myName: 'Ashwnai', myLastName: 'Pandey', email: 'abc@abc.com' }
+
+
+// Number 
+let myNum = 23
+let myNumTwo = 2.3
+let myStr = 'JavaScript'
+let myBool = true
+let myNull = null
+let myUndefined = undefined
+let mySym = Symbol("123")
+
+console.log([
+    typeof myNum,
+    typeof myNumTwo,
+    typeof myStr,
+    typeof myBool,
+    typeof myNull,
+    typeof myUndefined,
+    typeof mySym
+]);
